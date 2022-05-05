0f3d03e52c9e111cc03600192d0a881129488551
sanyam238 <82820362+sanyam238@users.noreply.github.com>
2022-05-05 13:16:10 +0400

    Add files via upload

---
+++ lets_chat.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCPgQts673r8NYH39vjH_mlF0yFurHbyHA",
+    authDomain: "lets-chat-c796f.firebaseapp.com",
+    projectId: "lets-chat-c796f",
+    storageBucket: "lets-chat-c796f.appspot.com",
+    messagingSenderId: "1087743355680",
+    appId: "1:1087743355680:web:da1933e4dfde44018a8545"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
